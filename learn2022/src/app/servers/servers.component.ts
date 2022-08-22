a0227b993eeadb6bdf42f69078687b2b19b33f4f
nikesh pathania <nikesh.pathania@gmail.com>
2022-08-22 22:18:49 +0530

    Property Binding vs String Interpolation

--- learn2022/src/app/servers/servers.component.ts
+++ learn2022/src/app/servers/servers.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowserver = false;
+
+  constructor() { 
+    setTimeout(() => {
+      this.allowserver = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
   }
